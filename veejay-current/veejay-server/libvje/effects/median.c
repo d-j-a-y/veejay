c30a9ce00451aa9b2bf06d3453c3131e6ce237a8
[d.j.a.y] Jerome Blanchi <d.j.a.y@free.fr>
2016-12-19 17:09:24 +0100

    vje / OpenMP simple test (Constant Time Median Filter)
    Result:
    -------
    The OpenMP overhead cost is higher to distributed calculation gain!

--- veejay-current/veejay-server/libvje/effects/median.c
+++ veejay-current/veejay-server/libvje/effects/median.c
@@ -22,6 +22,9 @@
 #include <libvjmem/vjmem.h>
 #include <ctmf/ctmf.h>
 #include "median.h"
+#include <omp.h>
+
+//#include <libvjmsg/vj-msg.h>
 
 vj_effect *medianfilter_init(int w, int h)
 {
@@ -47,23 +50,67 @@ void medianfilter_apply( VJFrame *frame, int val)
 	const unsigned int width = frame->width;
 	const unsigned int height = frame->height;
 	const int len = frame->len;
+/*
 	uint8_t *Y = frame->data[0];
 	uint8_t *Cb = frame->data[1];
 	uint8_t *Cr = frame->data[2];
+*/
 
 	if( val == 0 )
 	   return; 
 
+/*
 	 uint8_t *buffer = (uint8_t*) vj_malloc(sizeof(uint8_t)*len*3);
 	 veejay_memset( buffer,0, len*3);
+*/
+/*
 	 ctmf( Y, buffer, width,height,width,width,val,1,1024*1024*8);
 	 ctmf( Cb,buffer + (len), width,height/2,width,width,val,1,512*1024);
 	 ctmf( Cr,buffer + (len*2), width,height/2,width,width,val,1,512*1024);
+*/
 
+	#pragma omp parallel sections
+	{
+		#pragma omp section
+		{
+			uint8_t *buffer = (uint8_t*) vj_malloc(sizeof(uint8_t)*len);
+			veejay_memset( buffer,0, len);
+			uint8_t *Y = frame->data[0];
+
+//			 veejay_msg(VEEJAY_MSG_INFO, "Y %d ", omp_get_thread_num ( )) ;
+			ctmf( Y, buffer, width,height,width,width,val,1,1024*1024*8);
+			veejay_memcpy( Y, buffer, len);
+			free(buffer);
+		}
+		#pragma omp section
+		{
+			uint8_t *buffer = (uint8_t*) vj_malloc(sizeof(uint8_t)*len);
+			veejay_memset( buffer,0, len);
+			uint8_t *Cb = frame->data[1];
+
+//			veejay_msg(VEEJAY_MSG_INFO, "Cb %d ", omp_get_thread_num ( )) ;
+			ctmf( Cb,buffer , width,height/2,width,width,val,1,512*1024);
+			veejay_memcpy( Cb,buffer, len);
+			free(buffer);
+		}
+		#pragma omp section
+		{
+			uint8_t *buffer = (uint8_t*) vj_malloc(sizeof(uint8_t)*len);
+			veejay_memset( buffer,0, len);
+			uint8_t *Cr = frame->data[2];
+
+//			veejay_msg(VEEJAY_MSG_INFO, "Cr %d ", omp_get_thread_num ( )) ;
+			ctmf( Cr,buffer, width,height/2,width,width,val,1,512*1024);
+			veejay_memcpy( Cr,buffer, len);
+			free(buffer);
+		}
+	}
+
+/*
 	 veejay_memcpy( Y, buffer, len);
 	 veejay_memcpy( Cb,buffer + (len), len);
 	 veejay_memcpy( Cr,buffer + (len*2), len);
-	 
-	 free(buffer);
+*/
+//	 free(buffer);
 
 }
